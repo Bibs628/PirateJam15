e0deacfef18ef9ce45b1f9f956d27b040bd1bbd5
SootDan <destdanny@proton.me>
2024-07-22 20:33:52 +0200

    Q + E Elementwechsel
    Funktioniert, aber wird in der UI noch nicht angezeigt.

--- Script/GUI.gd
+++ Script/GUI.gd
@@ -2,8 +2,7 @@ extends Control
 
 func _ready():
 	var alch_status = get_container("AlchStatus")
-	for i in range(alch_status.size()):
-		alch_status[i].visible = i == 0
+	#alch_status.
 
 
 func health_bar_manager(health_points):
@@ -14,8 +13,8 @@ func health_bar_manager(health_points):
 
 func alchemist_status_manager(status):
 	var alch_status = get_container("AlchStatus")
-	for i in range(alch_status.size()):
-		alch_status[i].visible = i == status
+	#for i in range(alch_status.size()):
+	#	alch_status[i].visible = i == status
 
 func monster_bar_manager(health_points):
 	var monster_hp = get_container("MobHP")
--- Script/Player.gd
+++ Script/Player.gd
@@ -1,13 +1,26 @@
 extends Entity
 # Script fuer den Alchemisten.
 
-@export var element = alch_status
+@export_group("Elemente")
+@export var current_element = element.FIRE
+@export var element_speed = {
+	element.FIRE: 400,
+	element.PLANT: 500,
+	element.WIND: 600,
+	element.EARTH: 700,
+}
+@export var element_jump_velocity = {
+	element.FIRE: -400,
+	element.PLANT: -400,
+	element.WIND: -400,
+	element.EARTH: -4000
+}
 
 signal alchemist_status
 signal alchemist_hp(hp: int)
 signal alchemist_died
 
-enum alch_status {
+enum element {
 		FIRE, 
 		PLANT, 
 		WIND,
@@ -15,21 +28,32 @@ enum alch_status {
 }
 
 func _init():
-	super._init("Spieler")
-	print("Spieler spawned, %s hp." % health_points)
+	print("Spieler spawned, %s hp." % [health_points])
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Element Left"):
+		change_element((current_element - 1 + element.size()) % element.size())
+		
+	if Input.is_action_just_pressed("Element Right"):
+		change_element((current_element + 1 + element.size()) % element.size())
 
 
 func _physics_process(delta):
-	if position.x < 0: position.x = 0
+	if position.x < 0: 
+		position.x = 0
 		
 	if position.y > 1080:
 		_take_damage(3)
 		position.y = 1079
 		
-	if health_points <= 0: velocity = Vector2.ZERO
-	else: move_and_slide()
+	if health_points <= 0: 
+		velocity = Vector2.ZERO
+	else: 
+		move_and_slide()
 		
-	if not is_on_floor(): velocity.y += gravity * delta
+	if not is_on_floor(): 
+		velocity.y += gravity * delta
 	
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = jump_velocity
@@ -53,5 +77,9 @@ func _take_damage(dmg: int):
 		emit_signal("alchemist_died")
 
 
-func change_alchemist(status: String):
+func change_element(status: int):
 	emit_signal("alchemist_status", status)
+	current_element = status
+	print("Element ist nun %s." % current_element)
+	speed = element_speed[status]
+	jump_velocity = element_jump_velocity[status]
