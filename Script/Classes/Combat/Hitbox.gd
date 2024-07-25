ebe54af09cefc74d8b98a811af0ab551a17634e4
SootDan <destdanny@proton.me>
2024-07-25 21:31:18 +0200

    FireWeapon Testing
    Funktioniert immer noch nicht wie es soll, aber leicht verbessert.

--- Script/Classes/Combat/Hitbox.gd
+++ Script/Classes/Combat/Hitbox.gd
@@ -18,10 +18,16 @@ func timer_out():
 
 func throw_fireball(mouse: Vector2, max_fireball_distance: int):
 	visible = true
-	print(global_position)
 	var fireball = fire_weapon.instantiate()
-	var mouse_translate = Vector2(500, 500)
+	mouse = Vector2(500, 500)
+	'''var skp = global_position.dot(mouse)
+	var laenge = mouse.length() * global_position.length()
+	var winkel = skp / laenge
+	print("Winkel: %s" % winkel)'''
+	#global_position.angle_to_point(mouse)
+
 	fireball.weapon_position = global_position
-	fireball.weapon_rotation = global_position.angle_to(mouse_translate)
+	#fireball.weapon_rotation = winkel
+	fireball.weapon_rotation = global_position.angle_to_point(mouse)
 	print(fireball.weapon_rotation)
 	add_child.call_deferred(fireball)
