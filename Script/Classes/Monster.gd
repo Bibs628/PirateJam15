a0e082b6a487b63addd4c4be8d5806504ee3f4dc
SootDan <destdanny@proton.me>
2024-07-30 14:35:52 +0200

    Moveset + Hit Detection (WIP)

--- Script/Classes/Monster.gd
+++ Script/Classes/Monster.gd
@@ -5,9 +5,14 @@ var aware_of_player: bool = false
 var attacked_by_player: bool = false
 var fireball_position: Vector2
 var try_to_evade: bool = false
+var rng_movement: bool = true
+var random_target_position: Vector2
+
 @onready var raycast = $RayCast2D
 @onready var player_node = get_tree().get_nodes_in_group("Player")
 @onready var player = player_node[0]
+@onready var hurtbox = $Hurtbox
+@export var raycast_distance: int = 250
 
 signal monster_hp(hp: int)
 signal monster_died
@@ -18,15 +23,21 @@ func _init():
 
 
 func _ready():
-	raycast.target_position = Vector2(0, 250)
+	raycast.target_position = Vector2(0, raycast_distance)
+	#raycast.add_exception(player)
+	hurtbox.connect("area_entered", take_damage)
+	$Timer.start()
 	
 
 func _physics_process(delta):
 	super._physics_process(delta)
 	if aware_of_player:
 		position += (player.position - position) / speed
-	else:
+	#else:
+	#	random_movement(delta)
+	elif rng_movement:
 		position.x += randi_range(-speed, speed) / float(0.5 * speed)
+		rng_movement != rng_movement
 
 	if health_points <= 0:
 		monster_died.emit()
@@ -37,19 +48,33 @@ func _physics_process(delta):
 	if try_to_evade:
 		position += (fireball_position - position) / speed
 		try_to_evade = false
-		print("guck")
 	
 	if attacked_by_player and aware_of_player:
-		print(position.x)
-		position.x += 3* (fireball_position.x - position.x) / float(speed)
-		print(position.x)
+		position.x += 3 * (fireball_position.x - position.x) / float(speed)
 		try_to_evade = true
 		attacked_by_player = false
 
 
+func random_movement(delta):
+	#TODO: Code funktioniert einfach nicht
+	var direction = (random_target_position.x - position.x)
+	position.x += direction / speed * delta
+	
+	if abs(position.x - random_target_position.x) <= 1:
+		rng_movement = false
+
+
+func random_movement_timer():
+	if not aware_of_player:
+		$Timer.start()
+		random_target_position.x = randf_range(-speed, speed)
+		rng_movement = true
+	pass
+
 
 func take_damage(dmg: int):
 	super.take_damage(dmg)
+	print("SUSSY")
 	monster_hp.emit(dmg)
 
 
@@ -57,19 +82,12 @@ func flap_wings():
 	velocity.y = jump_velocity
 
 
-'''func try_to_evade(mouse: Vector2):
-	if mouse.x > position.x:
-		position.x += speed
-	else:
-		position.x -= speed'''
-
-
 func combat_logic():
 	#TODO: KI
 	pass
 
 
-func _on_awareness_body_entered(body):
+func on_awareness_body_entered(body):
 	if not aware_of_player and body == player:
 		aware_of_player = true
 		print("Monster sieht den Spieler!")
